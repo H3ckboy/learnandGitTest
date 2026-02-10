ecd209c4b972214e49aed82fc258f8a799878331
Cody B. <boggsc716@gmail.com>
2026-02-10 16:21:50 -0500

    teleop basics are here

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,40 +4,61 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//Bring SparkMAX library into the Subsystem
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+
+//Bringing different levels of the Constants files in the Subsystem
+import frc.robot.Constants.MotorConstants;
+import frc.robot.Constants;
+
 public class ExampleSubsystem extends SubsystemBase {
+
+    //create a new motor with the CANBus ID 9, and a "brushless" motor type (NEO is always brushless)
+    private final SparkMax exampleMotor = new SparkMax(9, MotorType.kBrushless);
+    //creates a new motor with the CANBus ID 10, using kNEO varible created in Constants
+    private final SparkMax testMotor = new SparkMax(10, MotorConstants.kNEO);
+
+
+    public RelativeEncoder testMotorEncoder = testMotor.getEncoder();
+
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    /* Applying the motor Configs set up in Constants file.
+     * 
+    */
+    exampleMotor.configure(
+      Constants.kTwntyAmpConfig, 
+      ResetMode.kResetSafeParameters, 
+      PersistMode.kPersistParameters);
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+    testMotor.configure(
+      Constants.kFrtyAmpConfig, 
+      ResetMode.kResetSafeParameters, 
+      PersistMode.kPersistParameters);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  //Creates a function that controls the speed of a motor on a scale of 0.0 (0%) - 1.0 (100%)
+  public void motorSpeed(double motorSpeed){
+    exampleMotor.set(motorSpeed);
+  }
+
+  //Creates a function that reads the encoder value of the testMotor
+  public double encoderReading(){
+    double testTicks = testMotorEncoder.getPosition();
+
+    return testTicks;
   }
 
   @Override
